4f160e678d1e1d39d6ea1c8b7908bede08e8fc78
romanserk <romanserk@gmail.com>
2019-02-19 02:58:02 +0200

    setting up the first event listeners

--- main.js
+++ main.js
@@ -17,6 +17,25 @@ var UIController = (function () {
 
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var ctrlAddItem = function () {
+
+        // 1. get the fielf input data
+        // 2. add the item to the budget controller
+        // 3. add the item to the UI
+        // 4. calculate the budget
+        // 5. display the budget on the UI
+
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+    });
 
 
 })(budgetController, UIController);
